32d3f2bbc98ac0ffa293ff1ed58458de23479ce7
Hamza <hamza40nazir@gmail.com>
2020-08-05 23:36:51 +0500

    Different codes for Clicking Label

--- CrossMail/CrossMail.Tests/GMailTests.cs
+++ CrossMail/CrossMail.Tests/GMailTests.cs
@@ -77,18 +77,9 @@ namespace CrossMail.Test
 
              //_browserDriver.FindElement(By.XPath("//*[text() ='Social']")).Click();  // This opens social tab
     
-            _browserDriver.FindElement(By.XPath("//div[@class='J-LC' and @title = 'Social']")).Click(); // trying to click 'Social' label
-            System.Threading.Thread.Sleep(1000);
-
-            /* 
-           IWebElement inputBar = _browserDriver.FindElement(By.XPath("//input[@aria-label='Label-as menu open']")); // Finding Search bar
-           inputBar.SendKeys("Social");    // Writing Social to look for it
-           inputBar.SendKeys(Keys.Enter);  // Pressing Enter in the end to select it    
-           System.Threading.Thread.Sleep(1000);
-         */
             try
             {
-                _browserDriver.FindElement(By.XPath("//input[@aria-label='Label-as menu open']")).SendKeys("Social");
+                _browserDriver.FindElement(By.XPath("//div[@class='J-LC'][@name = 'Social']")).Click(); // trying to click 'Social' label
             }
             catch(Exception e)
             {
@@ -97,7 +88,7 @@ namespace CrossMail.Test
 
             try
             {
-                _browserDriver.FindElement(By.ClassName("bqf")).SendKeys("Social");
+                _browserDriver.FindElement(By.Id(":w0")).Click(); // trying to click 'Social' label
             }
             catch (Exception e)
             {
@@ -106,12 +97,59 @@ namespace CrossMail.Test
 
             try
             {
-                _browserDriver.FindElement(By.XPath("//input[@maxlength='225']")).SendKeys("Social");
+                _browserDriver.FindElement(By.XPath("//div[@id = ':w0']")).Click(); // trying to click 'Social' label
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
+            try
+            {
+                _browserDriver.FindElement(By.XPath("//div[test() = 'Social']")).Click(); // trying to click 'Social' label
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                _browserDriver.FindElement(By.XPath("//div[@title = 'Social']")).Click(); // trying to click 'Social' label
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            /*
+                        try
+                        {
+                            _browserDriver.FindElement(By.XPath(".//*[@aria-label='Send']")).Click();
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+
+                        try
+                        {
+                            _browserDriver.FindElement(By.Id(":1hw")).Click();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+
+                        try
+                        {
+                            _browserDriver.FindElement(By.ClassName("T-I J-J5-Ji aoO v7 T-I-atl L3")).Click();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+            */
+            _browserDriver.Close();
 
 
         }
